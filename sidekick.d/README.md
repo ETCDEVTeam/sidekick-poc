f63a4d0a24f4162362a35efbf225081668f0cbc5
ia <isaac.ardis@gmail.com>
2018-04-26 14:10:15 +0900

    hopefully fix fucking submodule sidekick.d

---
+++ sidekick.d/README.md
@@ -0,0 +1,67 @@
+# ETCDEVTeam/sidekick-*
+
+> A collection of scripts and documents outlining requirements and initial adhoc solves for a minimum-viable ETC sidechains implementation.
+
+- [github.com/ETCDEVTeam/sidekick-tx2poa](http://github.com/ETCDEVTeam/sidekick-tx2poa). A PoA mechanism implemented through an emphemeral JS console.
+
+- [github.com/ETCDEVTeam/sidekick-liaison](http://github.com/ETCDEVTeam/sidekick-liaison). A bash script that listens to a sidechain node and facilitates communication with an arbitrary mainnet node. As written, relies on [emerald-cli](https://github.com/ETCDEVTeam/emerald-cli).
+
+- [github.com/ETCDEVTeam/sidekick-checkpointer](http://github.com/ETCDEVTeam/sidekick-checkpointer). A checkpointing mechanism implemented through an ephemeral JS console.
+
+# sidekick-tx2poa: Transaction+Header PoA
+
+The main idea is that nodes designated as [./authorities.js](./authorities.js) by configuration act as "authorized miners," and post a new "incomplete proof of authority transaction" for each new block added to the chain. This transaction contains an incomplete chunk of the miner's signature of the previous block's hash, which together with the rest of the signature found in the winning miner's block's `extraData` field can be used to verify that the block was indeed mined by an authoritative miner.
+
+This method of determining a PoA consensus does not require any protocol changes and utilizes only pre-existing tools and methods. Like a protocol-based PoA, it assumes that all nodes in the sidechain network can run equivalent or compatible configurations and agree on a list of pre-configured "authority" nodes described by public key addresses.
+
+### Details
+
+Authority nodes assert a proof of their identity by creating a signature `S` made by the authority's private key `Mpriv` of the previous block's hash `H`.
+
+```
+S = eth.sign(Mpub, H)
+```
+
+Since signatures are 65 bytes long, they're too big to fit into a block header's `extraData` field limited to 32 bytes. Instead, `S` is "chunked" into `s1` and `s2`, where as a concatenated string `s1+s2 == S`. As-is, `s1` has length of 8 and `s2` has a string length of 124 including the `0x` prefix, but this is a mostly arbitrary number within field size limit bounds.
+
+`s1` is then set by the authority node `M` as it's `extraData` field value, and `s2` is included as transaction `data` in a transaction that is created and broadcasted to the network for each new block.
+
+If miner `M` wins the block, any node on the network can verify it's authority by using `EcRecover` to verify that
+
+```
+s1 = currentBlock.extraData.substring(8); // chunk1
+s2 = poaTx.sig // chunk2
+S = s1 + s2;
+personal.ecRecover(H, S) == Mpub == currentBlock.miner == poaTx.From;
+```
+
+where together the transaction and block header contain the valid signature for the previous block's hash as made by `Mpriv`.
+
+If a block fails this PoA/Tx validation, the block is simply purged.
+
+### TODO
+- [x] geth needs a `personal.ecRecover` IPC API point to verify a given signature. https://github.com/ethereumproject/go-ethereum/pull/566
+
+### Run
+
+1. Edit `authorities.js` to include public keys for nodes that should have authority. This list should be identical for each node in the network.
+2. Ensure `sidenet/chain.json` configuration specifies `"consensus": "ethash-test"` unless you really like burning electricity.
+3. You'll probably want to set up your authority nodes as bootnodes in the config as well.
+4. Run:
+```
+$ geth --chain=sidenet --ipc-api="personal,miner,eth,web3,debug" --js-path="./sidekick-tx2poa" js path/to/authority.js
+
+OR
+
+$ geth --chain=sidenet --ipc-api="eth,web3,debug" --js-path="./sidekick-tx2poa" js path/to/minion.js
+```
+
+### Notes
+
+1. I'm not sure if this'll actually work.
+2. I don't know if it will be very scalable.
+3. It depends on using `tx.data[|input]` in a hacky way; instead of using it as compiled contract code it just uses it as a JSON messenging service.
+4. Depends on geth making the following IPC modules available `--ipc-apis=personal,miner,eth,web3,debug`.
+5. We have to use transactions instead of just header `extraData` because that field is limited to 32 bytes and signature hashes are 65. This is kind of annoying because it would be a lot simpler to just include the signature in the header.
+6. It's far faster to use an already-unlocked account for the authorities to sign block hashes. However, this compromises the public `eth` module from being used as a public RPC endpoint. This is a bummer. It means that only Minion nodes are safe to use as RPC endpoints.
+7. `personal` and `miner` IPC modules only need to be enabled for authority nodes.
---
+++ sidekick.d/tx2poa.js
@@ -0,0 +1,191 @@
+// load authorities (array)
+// sets
+//   var authorities = [...];
+loadScript("authorities.js");
+
+// TODO: I think this might be slow. Is there a faster way?
+function findPoaTxData(block) {
+	for (var i = 0; i < block.transactions.length-1; i++) {
+		var txH = block.transactions[i];
+		// skip if tx hash doesn't match tx hash prefix from blockHeader.extraData field.
+		if (block.extraData.substring(0,8) !== txH.substring(0,8)) {
+			continue;
+		}
+		var tx = eth.getTransactionByHash(txH); // assume unlikely tx hash prefix collisions
+		if (tx.from !== block.miner) {
+			return false;
+		}
+		var data;
+		try {
+			data = JSON.parse(web3.toAscii(tx.data));
+		} catch(err) {
+			console.log("invalid PoA Tx data for Tx:", tx);
+			return false;
+		}
+		if ((typeof data.sig === "String") && (typeof data.enode === "String")) {
+			return data;
+		}
+	}
+	return false;
+}
+
+// validateTxAuthority validates the authority of a block
+// it returns false if invalid, true if valid
+function validateAuthorityByTransaction(block) {
+	// gimmes and sanity checks
+	//
+	// genesis block is automatically OK
+	if (block.number === 0) {
+		return true;
+	}
+	// fail if the block miner (etherBase) is not an established authority
+	var authorityIndex = authorities.indexOf(block.miner);
+	if (authorityIndex < 0) {
+		return false;
+	}
+	// fail if block does not contain a sufficient poa tx
+	var txdata = findPoaTxData(block);
+	if (txdata === false) {
+		return false;
+	}
+	// here's the real poa; the rest could easily be forged
+	var ok = personal.ecRecover(eth.getBlock(block.number-1).hash, block.extraData.substring(8)+txdata.sig) == block.miner;
+	if (!ok) {
+		// admin.dropPeer(data.enode); // this could be forged easily... hm. There might be a way to be sure of the enode with another signature if it's worth it.
+		// authorities.splice(authorityIndex, 1);
+		console.log("invalid signature made apparently by". block.miner);
+		return ok; // false
+	}
+	return ok;
+}
+
+// ensureOrIgnoreBlockAuthority validates the current block's authority.
+// if validation fails, the block is purged and the function returns false.
+// if validation succeeds, the function returns true.
+function ensureOrIgnoreCurrentBlockAuthority() {
+	var b = eth.getBlock(eth.blockNumber);
+	if (!validateAuthorityByTransaction(b)) {
+		console.log("tx2poa", "VALIDATE", "FAIL", b);
+		debug.setHead(eth.blockNumber-1);
+		return false;
+	}
+	console.log("tx2poa", "VALIDATE", "OK", b.number);
+	return true;
+}
+
+var txObj = {}; // tx obj of last poa tx
+var tx = ""; // hash of last poa tx
+
+function postAuthorityDemonstration() {
+	var lastBlockN = 0; // init case as genesis
+	if (eth.blockNumber > 0) {
+		lastBlockN = eth.blockNumber - 1;
+	}
+	var lastBlock = eth.getBlock(lastBlockN); // previous block
+	var authorityAccount = eth.accounts[0];
+
+	var sig = eth.sign(authorityAccount, lastBlock.hash);
+
+	// This is the important part.
+	// Without splitting the signature hash (and, say, including the whole hash in the tx input field),
+	// authority could easily be forged by an attacker node that would just watch transactions and set header
+	// data from random authorities' transaction data. I guess it could successfully forge 1/authorities.length blocks.
+	// However, by splitting the signature between tx and header, the signature beocmes
+	// unknowable and thus unforgeable until the block is mined and broadcasted.
+	// eg. "0x3f2c6d378852d4e98c823d1d09e89e0ec5fffbe0d615b408b3a5dfcbaaf5a2e71800a98426e181a4e4945a9d910fe7a2471498c16f266bbd6bb3110318dd75601b"
+	var sigHeaderChunk = sig.substring(0,8); // firstchunk: "0x3f2c6d", smaller because field size limit
+	var sigTxChunk = sig.substring(8) // secondchunk: "378852d4e98c823d1d09e89e0ec5fffbe0d615b408b3a5dfcbaaf5a2e71800a98426e181a4e4945a9d910fe7a2471498c16f266bbd6bb3110318dd75601b"
+	// => sigHeaderChunk+sigTxChunk = signature hash
+
+	// post transaction as an authority
+	var d = JSON.stringify({
+		"sig": sigTxChunk,
+		"enode": admin.nodeInfo.enode
+	});
+	txObj = {
+		from: authorityAccount,
+		to: authorityAccount,
+		value: web3.toWei(1, 'wei'),
+		// use JSON just because we can and it seems extensible
+		data: web3.fromAscii(d)
+	};
+	tx = eth.sendTransaction();
+	// include this tx hash within 'extraData' in block if our authoritative miner wins.
+	// prefix sigHeaderChunk with tx hash substring for single-query validation.
+	if (!miner.setExtra(tx.substring(0,8)+sigHeaderChunk)) {
+		console.log("tx2poa", "AUTHORITY", "ERROR", "failed to set miner extra", tx, "becoming a Minion instead...");
+		tx = "err";
+		miner.stop(); // TODO: handle me better maybe
+	} else {
+		console.log("tx2poa", "AUTHORITY", "OK", tx, txObj);
+	}
+}
+
+// ensure there is an account and that it is unlocked
+function ensureAuthorityAccount() {
+	var authorityAccount;
+	if (eth.accounts.length === 0) {
+		  // FIXME: I0426 14:02:10.914469 consolecmd.go:200] Failed to execute /Users/ia/dev/sidekick/poc/sidekick.d/authority.js: ReferenceError: 'exit' is not defined
+	    // exit; // sanity check
+			console.log("tx2poa", "AUTHORITY", "ERR NO ACCOUNTS", "Running as minion instead...");
+			runMinion();
+	} else {
+		// Could improve so authority accounts could arbitrary account A from n accounts
+		authorityAccount = eth.accounts[0];
+	}
+	// TODO: dont have your password be yes, and dont pass it here
+	if (!personal.unlockAccount(authorityAccount, "yes", 0)) {
+			console.log("tx2poa", "AUTHORITY", "ERR ACCT LOCKED", "Running as minion instead...");
+			runMinion();
+		// FIXME: see above about exit not being defined
+		// exit;
+	}
+	miner.setEtherbase(authorityAccount);
+	console.log("tx2poa", "AUTHORITY", "INIT", authorityAccount);
+	return true;
+}
+
+// runAuthority runs recursively and continuously asserts the authority of a node
+// by sending a transaction to itself per block. If the node's miner wins the block,
+// the partial hash of that transaction's poa is included in block's 'extraData' field.
+// The function also validates the authority of all incoming blocks.
+// FIXME: it might block the normal shutdown mechanism for a geth client
+function runAuthority() {
+	if (tx === "err") {
+		admin.sleepBlocks(1);
+		runMinion();
+	}
+	if (ensureOrIgnoreCurrentBlockAuthority()) {
+		postAuthorityDemonstration();
+	} else {
+		// most recent block was invalid and was purged
+		// check if latest poa tx is still pending or was included in the block that was purged
+		var pending = eth.pendingTransactions();
+		var reuseTx = false;
+		if (pending.length > 0) {
+			for (var i = 0; i < pending.length-1; i++) {
+				if (pending[i].hash === tx) {
+					reuseTx = true;
+					break;
+				}
+			}
+		}
+		// if poa tx was not included and thus removed with the purged invalid block, resend it
+		if (reuseTx) {
+			console.log("tx2poa", "AUTHORITY", "RESENDING", txObj);
+			eth.resend(txObj);
+		} else {
+			// otherwise just post a new poa tx
+			postAuthorityDemonstration();
+		}
+	}
+	admin.sleepBlocks(1);
+	runAuthority();
+}
+
+// runMinion validates the authority of all incoming blocks.
+function runMinion() {
+	ensureOrIgnoreCurrentBlockAuthority();
+	admin.sleepBlocks(1);
+	runMinion();
+}
