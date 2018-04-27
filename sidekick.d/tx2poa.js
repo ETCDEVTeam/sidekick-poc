677395c9c66ee87ef44de03f2af8c790358a9092
ia <isaac.ardis@gmail.com>
2018-04-27 13:52:48 +0900

    save

--- sidekick.d/tx2poa.js
+++ sidekick.d/tx2poa.js
@@ -1,38 +1,73 @@
+
+
+var cacheAuthorityOrMinion = "";
+function mustAmMinionOrAuthority() {
+	if (cacheAuthorityOrMinion !== "") {return cacheAuthorityOrMinion; }
+	cacheAuthorityOrMinion = (eth.accounts.length > 0 && authorities.indexOf(eth.accounts[0]) >= 0) ? "AUTHORITY" : "MINION";
+	return cacheAuthorityOrMinion;
+}
+
+function logWithPrefix(s) {
+	console.log(mustAmMinionOrAuthority() + "@[" + admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr +"]\n", s);
+}
+
+function logStatus(action, state, reason, detailsObj) {
+	var s = "\n    " + action + ": " + state;
+	if (reason !== null) {s += "(" + reason + ")"; }
+	s += "\n";
+	for (k in detailsObj) {
+		if (detailsObj.hasOwnProperty(k)) {
+			var v = detailsObj[k];
+			if (typeof v === Array) {
+				v = v.join(", ");
+			}
+			if (typeof v === Object) {
+				v = JSON.stringify(v);
+			}
+			s += "\n    " + k + ": " + v;
+		}
+	}
+	logWithPrefix(s);
+}
+
+
 function findPoaTxData(block) {
+	var out = {
+		ok: false,
+		error: "",
+		data: {}
+	};
 	for (var i = 0; i < block.transactions.length; i++) {
 		var txH = block.transactions[i];
 		// skip if tx hash doesn't match tx hash prefix from blockHeader.extraData field.
-
-		// FIXME
-		// 5ddc1f[::]:30311 tx2poa VALIDATE FAIL(NO_MATCH_TX) bExtra: 0x153406 txs: 0x153406332a082c1db29c2b7c1e2afcd950afde222394a42dc011524324f1a122
-
 		var bed = block.extraData.substring(0,8);
 		var txsub = txH.substring(0,8);
 		if (bed !== txsub) {
-			console.log(bed, "!=", txsub);
 			continue;
 		}
 
 		var tx = eth.getTransaction(txH); // assume unlikely tx hash prefix collisions
 
 		if (tx["from"] !== block["miner"]) {
-			console.log("tx.from != block.miner", tx.from, block.miner);
-			return false;
+			out.error = "tx.from != block.miner";
+			return out;
 		}
 
 		var data;
 		try {
 			data = JSON.parse(web3.toAscii(tx.input));
 		} catch(err) {
-			console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "invalid PoA Tx data for Tx:", JSON.stringify(tx));
-			return false;
+			out.error = "invalid PoA Tx data";
+			return out;
 		}
 		// if ((typeof data.sig === "String") && (typeof data.enode === "String")) {
-			return data;
+			out.ok = true;
+			out.data = data;
+			return out;
 		// }
 	}
-	console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "VALIDATE", "FAIL(NO_MATCH_TX)", "bExtra:", block.extraData.substring(0,8), "txs:", block.transactions.join(", "));
-	return false;
+	out.error = "no matching tx from header";
+	return out;
 }
 
 // validateTxAuthority validates the authority of a block
@@ -42,33 +77,65 @@ function validateAuthorityByTransaction(block) {
 	//
 	// genesis block is automatically OK
 	if (block.number === 0) {
-		console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "VALIDATE", "OK(GENESIS)", block.number);
+		logStatus("VALIDATE", "SUCCESS", "genesis", {
+			"block_number": block.number,
+			"block_hash": block.hash.substring(0,8)+"..."
+		});
 		return true
 	}
+
 	// fail if the block miner (etherBase) is not an established authority
 	var authorityIndex = authorities.indexOf(block.miner);
 	if (authorityIndex < 0) {
-		console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "VALIDATE", "FAIL(BLOCK_MINER)", block.miner, "!<", authorities.join(", "));
+		logStatus("VALIDATE", "FAIL", "miner not authorized", {
+			"eth_blockNumber": eth.blockNumber,
+			"block_number": block.number,
+			"block_hash": block.hash.substring(0,8)+"...",
+			"block_miner": block.miner,
+			"authorities": authorities
+		});
 		return false;
 	}
+
 	// fail if block does not contain a sufficient poa tx
-	var txdataJSON = findPoaTxData(block);
-	if (txdataJSON === false) {
-		console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "VALIDATE", "FAIL(TXDATA)");
+	var poaTxData = findPoaTxData(block);
+	if (!poaTxData.ok) {
+		logStatus("VALIDATE", "FAIL", poaTxData.error, {
+			"eth_blockNumber": eth.blockNumber,
+			"block_number": block.number,
+			"block_hash": block.hash,
+			"block_transactions": block.transactions,
+			"block_miner": block.miner,
+		});
 		return false;
 	}
+
 	// here's the real poa; the rest could easily be forged
-	var sig = "0x"+block.extraData.substring(8)+txdataJSON.sig;
-	console.log("    validating sig:",sig);
-	console.log("    block_number  :",txdataJSON.block_number);
-	var rec = personal.ecRecover(eth.getBlock(txdataJSON.block_number).hash, sig);
+	var sig = "0x"+block.extraData.substring(8)+poaTxData.data.sig;
+	var rec = personal.ecRecover(eth.getBlock(poaTxData.data.block_number).hash, sig);
+
 	var ok = rec === block.miner;
 	if (!ok) {
-		console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "VALIDATE", "FAIL(SIG)", "\n    recovered:", rec, "\n    miner:", block.miner, "\n    sig:", sig);
+		logStatus("VALIDATE", "FAIL", "invalid signature", {
+			"eth_blockNumber": eth.blockNumber,
+			"block_number": block.number,
+			"block_hash": block.hash.substring(0,8)+"...",
+			"signature": sig,
+			"recovered": rec,
+			"miner": block.miner
+		});
 		// admin.dropPeer(data.enode); // this could be forged easily... hm. There might be a way to be sure of the enode with another signature if it's worth it.
 		// authorities.splice(authorityIndex, 1);
 		return ok; // false
 	}
+	logStatus("VALIDATE", "SUCCESS", null, {
+		"eth_blockNumber": eth.blockNumber,
+		"block_number": block.number,
+		"block_hash": block.hash.substring(0,8)+"...",
+		"signature": sig,
+		"recovered": rec,
+		"miner": block.miner
+	});
 	return ok;
 }
 
@@ -76,28 +143,32 @@ function validateAuthorityByTransaction(block) {
 // if validation fails, the block is purged and the function returns false.
 // if validation succeeds, the function returns true.
 function ensureOrIgnoreCurrentBlockAuthority() {
-	var b = eth.getBlock(eth.blockNumber);
+	var bn = eth.blockNumber;
+	var b = eth.getBlock(bn);
 	if (!validateAuthorityByTransaction(b)) {
-		console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "VALIDATE", "FAIL", "block.number:", b.number);
-		debug.setHead(eth.blockNumber-1);
+		debug.setHead(bn-1);
 		return false;
 	}
-	console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "VALIDATE", "OK", "block.number:", b.number);
 	return true;
 }
 
 function postAuthorityDemonstration() {
 
+	// pause miner
 	miner.stop();
 
+	// initialize status pessimistically
 	var status = "fail";
+
+	// get the last valid block
 	var lastBlockN = 0; // init case as genesis
 	if (eth.blockNumber > 0) {
 		lastBlockN = eth.blockNumber - 1;
 	}
 	var lastBlock = eth.getBlock(lastBlockN); // previous block
-	var authorityAccount = eth.accounts[0];
 
+	// sign the last block hash
+	var authorityAccount = eth.accounts[0];
 	var sig = eth.sign(authorityAccount, lastBlock.hash);
 
 	// This is the important part.
@@ -111,31 +182,46 @@ function postAuthorityDemonstration() {
 	var sig_part2 = sig.substring(8) // secondchunk: "378852d4e98c823d1d09e89e0ec5fffbe0d615b408b3a5dfcbaaf5a2e71800a98426e181a4e4945a9d910fe7a2471498c16f266bbd6bb3110318dd75601b"
 	// => sig_part1+sig_part2 = signature hash
 
-	// post transaction as an authority
-	var d = JSON.stringify({
+	// assemble data to include in poaTx
+	var rawData = {
 		"sig": sig_part2,
 		"block_number": lastBlock.number,
 		"enode": admin.nodeInfo.enode
-	});
-	// var txObj = buildAuthTxObj();
+	};
+	var d = JSON.stringify(rawData);
+
 	var txObj = {
 		"from": authorityAccount,
 		"to": authorityAccount,
 		"value": web3.toWei(1, 'wei'),
-   	// use JSON just because we can and it seems extensible
+
+		// use JSON just because we can and it seems extensible
 		"data": web3.fromAscii(d).substring(2) // strip 0x
 	};
-	var tx = eth.sendTransaction(txObj);
-	console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "AUTHORITY", "POST PoTx", "\n    tx:", tx.substring(0,8), "\n    block.number:", eth.blockNumber, "\n    sig:", sig);
 
-	// include this tx hash within 'extraData' in block if our authoritative miner wins.
-	// prefix sig_part1 with tx hash substring for single-query validation.
+	// broadcast the poa transactions
+	var tx = eth.sendTransaction(txObj);
+	logStatus("AUTHORITY", "POST", null, {
+		"eth_blockNumber": eth.blockNumber,
+		"authority": authorityAccount,
+		"tx_hash": tx.substring(0,8)+"...",
+		"data": rawData,
+		"signature": sig
+	});
+
+	// set tx hash as miner.ExtraData in case our miner wins
+	// the tx substring part aids precision of validation by enabling QueryN=1 eth.getTransaction query instead of QueryN <= block.transactions.length
 	if (!miner.setExtra(tx.substring(0,8)+sig_part1)) {
-		console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "AUTHORITY", "ERROR", "failed to set miner extra", tx, "becoming a Minion instead...");
-		tx = "err";
+
+		logStatus("AUTHORITY", "ERROR", "failed to set miner extra data", {
+			"eth_blockNumber": eth.blockNumber,
+			"authority": authorityAccount,
+			"tx_hash": tx.substring(0,8)+"...",
+			"currentBlockNumber": eth.blockNumber
+		});
+
 		miner.stop(); // TODO: handle me better maybe
 	} else {
-		console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "AUTHORITY", "SET MINER EXTRA", tx, txObj.data.substring(0,4));
 		status = "success";
 	}
 
@@ -152,13 +238,14 @@ function postAuthorityDemonstration() {
 
 // ensure there is an account and that it is unlocked
 function ensureAuthorityAccount() {
-	console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "ensureing auth");
 	var authorityAccount;
 
 	if (eth.accounts.length === 0) {
 		// FIXME: method undefined error
 			// exit; // sanity check
-			console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "AUTHORITY", "ERROR", "no accounts", "running minion...")
+			logStatus("AUTHORITY", "ERROR", "no accounts exist", {
+				"eth_blockNumber": eth.blockNumber
+			});
 			runMinion();
 	} else {
 		// Could improve so authority accounts could arbitrary account A from n accounts
@@ -171,7 +258,19 @@ function ensureAuthorityAccount() {
 	// 	// exit;
 	// }
   var ok = miner.setEtherbase(authorityAccount);
-	console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "AUTHORITY", "INIT", authorityAccount, ok);
+	// console.log(admin.nodeInfo.id.substring(0,6)+admin.nodeInfo.listenAddr, "tx2poa", "AUTHORITY", "INIT", authorityAccount, ok);
+	if (ok) {
+		logStatus("AUTHORITY", "SUCCESS", "initialized", {
+			"eth_blockNumber": eth.blockNumber,
+			"etherbase": authorityAccount
+		});
+	} else {
+		logStatus("AUTHORITY", "ERROR", "could not set etherbase", {
+			"eth_blockNumber": eth.blockNumber,
+			"etherbase": authorityAccount
+		});
+	}
+
 	return ok;
 }
 
